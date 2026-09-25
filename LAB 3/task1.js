1711b8b9b8f2ca706b8206ebacceb343765200fb
Hassan Shahid <hassanshahid7834@gmail.com>
2026-09-25 08:57:22 +0500

    Created files and basic code in lab3

---
+++ LAB 3/task1.js
@@ -0,0 +1,9 @@
+console.log("Script Connected")
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+const taskCount = document.getElementById("taskCount");
+const errorMessage = document.getElementById("errorMessage");
+
+const value = taskInput.value
+console.log(value);
